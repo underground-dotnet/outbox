3f98f16ad9b22fc59732e0328d4103ab6d97235d
Nils Lattek <470938+NilsLattek@users.noreply.github.com>
2025-10-31 20:02:31 +0100

    docs: Example on how to customize outbox entity (#18)

--- example/ConsoleApp/AppDbContext.cs
+++ example/ConsoleApp/AppDbContext.cs
@@ -7,4 +7,10 @@ namespace ConsoleApp;
 sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options), IOutboxDbContext
 {
     public DbSet<OutboxMessage> OutboxMessages { get; set; }
+
+    // example on how to apply custom configuration to the outbox model
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new OutboxMessageEntityTypeConfiguration());
+    }
 }
